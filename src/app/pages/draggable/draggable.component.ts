c105806f947eff9d624a9766e5f430011b45af55
LaamGinghong <lanjinghang@banggood.com>
2018-11-26 15:47:51 +0800

    完成draggable式拖拽

--- src/app/pages/draggable/draggable.component.ts
+++ src/app/pages/draggable/draggable.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-draggable',
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./draggable.component.scss']
 })
 export class DraggableComponent implements OnInit {
+  @ViewChild('box') box: ElementRef;
+  mouseLeft: number;
+  mouseTop: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private renderer2: Renderer2
+  ) {
   }
 
+  ngOnInit(): void {
+  }
+
+  drag(e: MouseEvent): void {
+    this.mouseLeft = e.clientX - this.box.nativeElement.offsetLeft;
+    this.mouseTop = e.clientY - this.box.nativeElement.offsetTop;
+  }
+
+  drop(e: MouseEvent): void {
+    this.renderer2.setStyle(this.box.nativeElement, 'left', `${e.clientX - this.mouseLeft}px`);
+    this.renderer2.setStyle(this.box.nativeElement, 'top', `${e.clientY - this.mouseTop}px`);
+  }
 }
