c6f2f33a1fa78c97ab3be99eaff046566e4f3ca6
LaamGinghong <lanjinghang@banggood.com>
2018-11-26 16:12:36 +0800

    完成鼠标移动式拖动

--- src/app/pages/mouse/mouse.component.ts
+++ src/app/pages/mouse/mouse.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-mouse',
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mouse.component.scss']
 })
 export class MouseComponent implements OnInit {
+  @ViewChild('box') box: ElementRef;
+  dragging = false;
+  mouseLeft: number;
+  mouseTop: number;
 
-  constructor() { }
+  constructor(
+    private renderer2: Renderer2
+  ) {
+  }
 
   ngOnInit() {
   }
 
+  drag(e: MouseEvent): void {
+    if (e.target === this.box.nativeElement) {
+      this.dragging = true;
+      this.mouseLeft = e.clientX - this.box.nativeElement.offsetLeft;
+      this.mouseTop = e.clientY - this.box.nativeElement.offsetTop;
+    }
+  }
+
+  drop(): void {
+    this.dragging = false;
+  }
+
+  move(e: MouseEvent): void {
+    if (this.dragging) {
+      this.renderer2.setStyle(this.box.nativeElement, 'left', `${e.clientX - this.mouseLeft}px`);
+      this.renderer2.setStyle(this.box.nativeElement, 'top', `${e.clientY - this.mouseTop}px`);
+    }
+  }
 }
